a23717ebc01cdbec22dd6759772e31ab46ddb336
xiaorun2345 <15610499173@163.com>
2021-05-25 10:16:59 +0800

    Add files via upload

---
+++ yolov5_trt_gstreamer.py
@@ -0,0 +1,409 @@
+#coding=utf-8
+
+"""
+An example that uses TensorRT's Python api to make inferences.
+"""
+import ctypes
+import os
+import random
+import sys
+import threading
+import time
+
+import cv2
+import numpy as np
+import pycuda.autoinit
+import pycuda.driver as cuda
+import tensorrt as trt
+from config import *
+INPUT_W = 608
+INPUT_H = 608
+CONF_THRESH = 0.4
+IOU_THRESHOLD = 0.2
+global num
+num=0
+def NMS(dets,score, thresh):
+    #scores=score.numpy()
+    #dets=dets.numpy()
+    #x1、y1、x2、y2、以及score赋值
+    # （x1、y1）（x2、y2）为box的左上和右下角标
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = score
+
+
+    #每一个候选框的面积
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    #order是按照score降序排序的，得到的是排序的本来的索引，不是排完序的原数组
+    order = scores.argsort()[::-1]
+    # ::-1表示逆序
+
+    temp = []
+    while order.size > 0:
+        i = order[0]
+        temp.append(i)
+        #计算当前概率最大矩形框与其他矩形框的相交框的坐标
+        # 由于numpy的broadcast机制，得到的是向量
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        #计算相交框的面积,注意矩形框不相交时w或h算出来会是负数，需要用0代替
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        #计算重叠度IoU
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        #找到重叠度不高于阈值的矩形框索引
+        inds = np.where(ovr <= thresh)[0]
+        #将order序列更新，由于前面得到的矩形框索引要比矩形框在原order序列中的索引小1，所以要把这个1加回来
+        order = order[inds + 1]
+    return temp
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+    """
+    description: Plots one bounding box on image img,
+                 this function comes from YoLov5 project.
+    param: 
+        x:      a box likes [x1,y1,x2,y2]
+        img:    a opencv image object
+        color:  color to draw rectangle, such as (0,255,0)
+        label:  str
+        line_thickness: int
+    return:
+        no return
+
+    """
+    tl = (
+        line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1
+    )  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(
+            img,
+            label,
+            (c1[0], c1[1] - 2),
+            0,
+            tl / 3,
+            [225, 255, 255],
+            thickness=tf,
+            lineType=cv2.LINE_AA,
+        )
+
+
+class YoLov5TRT(object):
+    """
+    description: A YOLOv5 class that warps TensorRT ops, preprocess and postprocess ops.
+    """
+    
+    def __init__(self, engine_file_path):
+        # Create a Context on this device,
+        self.cfx = cuda.Device(0).make_context()
+        stream = cuda.Stream()
+        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+        runtime = trt.Runtime(TRT_LOGGER)
+
+        # Deserialize the engine from file
+        with open(engine_file_path, "rb") as f:
+            engine = runtime.deserialize_cuda_engine(f.read())
+        context = engine.create_execution_context()
+
+        host_inputs = []
+        cuda_inputs = []
+        host_outputs = []
+        cuda_outputs = []
+        bindings = []
+        Frame=[]
+        for binding in engine:
+            size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
+            dtype = trt.nptype(engine.get_binding_dtype(binding))
+            # Allocate host and device buffers
+            host_mem = cuda.pagelocked_empty(size, dtype)
+            cuda_mem = cuda.mem_alloc(host_mem.nbytes)
+            # Append the device buffer to device bindings.
+            bindings.append(int(cuda_mem))
+            # Append to the appropriate list.
+            if engine.binding_is_input(binding):
+                host_inputs.append(host_mem)
+                cuda_inputs.append(cuda_mem)
+            else:
+                host_outputs.append(host_mem)
+                cuda_outputs.append(cuda_mem)
+
+        # Store
+        self.stream = stream
+        self.context = context
+        self.engine = engine
+        self.host_inputs = host_inputs
+        self.cuda_inputs = cuda_inputs
+        self.host_outputs = host_outputs
+        self.cuda_outputs = cuda_outputs
+        self.bindings = bindings
+
+    def infer(self,frame):
+        threading.Thread.__init__(self)
+        # Make self the active context, pushing it on top of the context stack.
+        self.cfx.push()
+        # Restore
+        stream = self.stream
+        context = self.context
+        engine = self.engine
+        host_inputs = self.host_inputs
+        cuda_inputs = self.cuda_inputs
+        host_outputs = self.host_outputs
+        cuda_outputs = self.cuda_outputs
+        bindings = self.bindings
+        t1=time.time()
+        input_image, image_raw, origin_h, origin_w = self.preprocess_image(frame)
+        # Copy input image to host buffer
+        np.copyto(host_inputs[0], input_image.ravel())
+        # Transfer input data  to the GPU.
+        cuda.memcpy_htod_async(cuda_inputs[0], host_inputs[0], stream)
+        # Run inference.
+        context.execute_async(bindings=bindings, stream_handle=stream.handle)
+        # Transfer predictions back from the GPU.
+        cuda.memcpy_dtoh_async(host_outputs[0], cuda_outputs[0], stream)
+    # Synchronize the stream
+        stream.synchronize()
+        # Remove any context from the top of the context stack, deactivating it.
+        self.cfx.pop()
+        # Here we use the first row of output in that batch_size = 1
+        output = host_outputs[0]
+        # Do postprocess
+        result_boxes, result_scores, result_classid = self.post_process(
+                output, origin_h, origin_w
+        )
+        print("time: ",time.time()-t1)
+        print(result_boxes)
+        #Draw rectangles and labels on the original image
+        for i in range(len(result_boxes)):
+            tl = (round(0.002 * (image_raw.shape[0] + image_raw.shape[1]) / 2) + 1) 
+            tf = max(tl - 1, 1)  # font thickness
+            #line/font thickness
+            color = (0,255,255)
+            x=result_boxes[i]
+            c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+            cv2.rectangle(image_raw, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+            label="{}:{:.2f}".format(categories[int(result_classid[i])],result_scores[i])
+            cv2.putText(image_raw,label, (c1[0], c1[1] - 2),0,tl / 3,[225, 255, 255],thickness=tf,lineType=cv2.LINE_AA,)
+        #Save image
+        global num
+        #save_name="image/"+str(num)+".jpg"
+        if is_show:
+            cv2.imshow('frame', image_raw)
+            cv2.waitKey(1)
+        num+=1
+
+    def destroy(self):
+        # Remove any context from the top of the context stack, deactivating it.
+        self.cfx.pop()
+
+    def preprocess_image(self,frame):
+        """
+        description: Read an image from image path, convert it to RGB,
+                     resize and pad it to target size, normalize to [0,1],
+                     transform to NCHW format.
+        param:
+            input_image_path: str, image path
+        return:
+            image:  the processed image
+            image_raw: the original image
+            h: original height
+            w: original width
+        """
+        image_raw = frame
+        h, w, c = image_raw.shape
+        image = cv2.cvtColor(image_raw, cv2.COLOR_BGR2RGB)
+        # Calculate widht and height and paddings
+        r_w = INPUT_W / w
+        r_h = INPUT_H / h
+        if r_h > r_w:
+            tw = INPUT_W
+            th = int(r_w * h)
+            tx1 = tx2 = 0
+            ty1 = int((INPUT_H - th) / 2)
+            ty2 = INPUT_H - th - ty1
+        else:
+            tw = int(r_h * w)
+            th = INPUT_H
+            tx1 = int((INPUT_W - tw) / 2)
+            tx2 = INPUT_W - tw - tx1
+            ty1 = ty2 = 0
+        # Resize the image with long side while maintaining ratio
+        image = cv2.resize(image, (tw, th))
+        # Pad the short side with (128,128,128)
+        image = cv2.copyMakeBorder(
+            image, ty1, ty2, tx1, tx2, cv2.BORDER_CONSTANT, (128, 128, 128)
+        )
+        image = image.astype(np.float32)
+        # Normalize to [0,1]
+        image /= 255.0
+        # HWC to CHW format:
+        image = np.transpose(image, [2, 0, 1])
+        # CHW to NCHW format
+        image = np.expand_dims(image, axis=0)
+        # Convert the image to row-major order, also known as "C order":
+        image = np.ascontiguousarray(image)
+        return image, image_raw, h, w
+
+    def xywh2xyxy(self, origin_h, origin_w, x):
+        """
+        description:    Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+        param:
+            origin_h:   height of original image
+            origin_w:   width of original image
+            x:          A boxes tensor, each row is a box [center_x, center_y, w, h]
+        return:
+            y:          A boxes tensor, each row is a box [x1, y1, x2, y2]
+        """
+        y = np.zeros_like(x) 
+        #if isinstance(x, torch.Tensor) else np.zeros_like(x)
+        r_w = INPUT_W / origin_w
+        r_h = INPUT_H / origin_h
+        if r_h > r_w:
+            y[:, 0] = x[:, 0] - x[:, 2] / 2
+            y[:, 2] = x[:, 0] + x[:, 2] / 2
+            y[:, 1] = x[:, 1] - x[:, 3] / 2 - (INPUT_H - r_w * origin_h) / 2
+            y[:, 3] = x[:, 1] + x[:, 3] / 2 - (INPUT_H - r_w * origin_h) / 2
+            y /= r_w
+        else:
+            y[:, 0] = x[:, 0] - x[:, 2] / 2 - (INPUT_W - r_h * origin_w) / 2
+            y[:, 2] = x[:, 0] + x[:, 2] / 2 - (INPUT_W - r_h * origin_w) / 2
+            y[:, 1] = x[:, 1] - x[:, 3] / 2
+            y[:, 3] = x[:, 1] + x[:, 3] / 2
+            y /= r_h
+
+        return y
+
+    def post_process(self, output, origin_h, origin_w):
+        """
+        description: postprocess the prediction
+        param:
+            output:     A tensor likes [num_boxes,cx,cy,w,h,conf,cls_id, cx,cy,w,h,conf,cls_id, ...] 
+            origin_h:   height of original image
+            origin_w:   width of original image
+        return:
+            result_boxes: finally boxes, a boxes tensor, each row is a box [x1, y1, x2, y2]
+            result_scores: finally scores, a tensor, each element is the score correspoing to box
+            result_classid: finally classid, a tensor, each element is the classid correspoing to box
+        """
+        # Get the num of boxes detected
+        num = int(output[0])
+        # Reshape to a two dimentional ndarray
+        pred = np.reshape(output[1:], (-1, 6))[:num, :]
+        # to a torch Tensor
+        #pred = torch.Tensor(pred).cuda()
+        # Get the boxes
+        boxes = pred[:, :4]
+        # Get the scores
+        scores = pred[:, 4]
+        # Get the classid
+        classid = pred[:, 5]
+        # Choose those boxes that score > CONF_THRESH
+        si = scores > CONF_THRESH
+        boxes = boxes[si, :]
+        scores = scores[si]
+        classid = classid[si]
+        # Trandform bbox from [center_x, center_y, w, h] to [x1, y1, x2, y2]
+        boxes = self.xywh2xyxy(origin_h, origin_w, boxes)
+        # Do nms
+        indices=NMS(boxes,scores,IOU_THRESHOLD)
+        result_boxes = boxes[indices,:]
+        result_scores = scores[indices]
+        result_classid = classid[indices]
+        return result_boxes, result_scores, result_classid
+
+def open_cam_rtsp(cameraName):
+    gst_str = ('rtspsrc location={} latency={} ! '
+               'rtph264depay ! h264parse ! omxh264dec ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! '
+               'videoconvert ! appsink').format(cameraName, 200, 1080, 720)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER) 
+
+class RTSCapture(cv2.VideoCapture):
+    _cur_frame = None
+    _reading = False
+    schemes=["rtsp://","rtmp://"]
+    @staticmethod
+    def create(url,*schemes):
+        gst_str = ('rtspsrc location={} latency={} ! '
+               'rtph264depay ! h264parse ! omxh264dec ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! '
+               'videoconvert ! appsink').format(url, 200, 1280, 720)
+        rtscap = RTSCapture(gst_str,cv2.CAP_GSTREAMER)
+        rtscap.frame_receiver = threading.Thread(target=rtscap.recv_frame, daemon=True)
+        if isinstance(url, str) and url.startswith(tuple(rtscap.schemes)):
+            rtscap._reading = True
+        elif isinstance(url, int):
+            pass
+        return rtscap
+
+    def isStarted(self):
+        ok = self.isOpened()
+        if ok and self._reading:
+            ok = self.frame_receiver.is_alive()
+        return ok
+
+    def recv_frame(self):
+        while self._reading and self.isOpened():
+            ok, frame = self.read()
+            if not ok: break
+            self._cur_frame = frame
+        self._reading = False
+
+    def read2(self):
+        frame = self._cur_frame
+        self._cur_frame = None
+        return frame is not None, frame
+
+    def start_read(self):
+        self.frame_receiver.start()
+        self.read_latest_frame = self.read2 if self._reading else self.read
+
+    def stop_read(self):
+        self._reading = False
+        if self.frame_receiver.is_alive(): self.frame_receiver.join()
+
+if __name__ == "__main__":
+    camera_addr='rtsp://admin:mkls1123@192.168.0.64/'
+    
+    #cap=open_cam_rtsp(camera_addr,200,1280,720)
+    #cap=cv2.VideoCapture("1.mp4")
+    rtspCap=RTSCapture.create(camera_addr)
+    rtspCap.start_read()
+    # load custom plugins
+    PLUGIN_LIBRARY = "libmyplugins.so"
+    ctypes.CDLL(PLUGIN_LIBRARY)
+    engine_file_path = "yolov5s.engine"
+
+    # load coco labels
+
+    categories = ["person", "hat"]
+
+    # a  YoLov5TRT instance
+    yolov5_wrapper = YoLov5TRT(engine_file_path)
+
+    while rtspCap.isStarted:
+            ret,Frame=rtspCap.read_latest_frame()
+            if ret==True:
+                yolov5_wrapper.infer(Frame)
+
+            else:
+                continue
+    # destroy the instance
+    yolov5_wrapper.destroy()
+    #thread2.join()
